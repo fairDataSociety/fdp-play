145005ef653e45cca366fc8b1999c8cf1386055d
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2022-06-29 12:56:10 +0200

    fix: set blockchain image name with version

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_BLOCKCHAIN_IMAGE = 'fairdatasociety/fdp-play-blockchain'
+export const DEFAULT_BLOCKCHAIN_IMAGE = 'fairdatasociety/fdp-play-blockchain:1.2.0' // TODO make it dynamic
 export const DEFAULT_BLOCKCHAIN_CONTAINER = 'fdp-play-blockchain'
 export const ENV_ENV_PREFIX_KEY = 'FDP_PLAY_ENV_PREFIX'
