c3c8714e80f0ec9eb4b9ed4f45ed491d541fa0fd
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2022-07-07 12:25:29 +0200

    ci: add latest input for building docker images

--- generator/scripts/publish-environment.sh
+++ generator/scripts/publish-environment.sh
@@ -26,6 +26,22 @@ BEE_IMAGE_PREFIX=$("$MY_PATH/utils/env-variable-value.sh" BEE_IMAGE_PREFIX)
 BLOCKCHAIN_NAME="$BEE_ENV_PREFIX-blockchain"
 BLOCKCHAIN_IMAGE_NAME="$BEE_IMAGE_PREFIX/$BLOCKCHAIN_NAME:$BLOCKCHAIN_VERSION"
 
+LATEST=false
+# handle passed options
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        --latest)
+        LATEST=true
+        shift 1
+        ;;
+        *)
+        echoerr "Unknown argument: $1"
+        usage
+        ;;
+    esac
+done
+
 echo "Search Docker built images with the following parameters: $BEE_IMAGE_PREFIX/$BEE_ENV_PREFIX*:$BEE_VERSION"
 DOCKER_IMAGES=$(docker image ls --format "{{.Repository}}:{{.Tag}}" -af reference="$BEE_IMAGE_PREFIX/$BEE_ENV_PREFIX*:$BEE_VERSION")
 echo "Push Bee docker images: $DOCKER_IMAGES"
@@ -33,7 +49,17 @@ for DOCKER_IMAGE in $DOCKER_IMAGES
 do
   echo "$DOCKER_IMAGE"
   docker push "$DOCKER_IMAGE"
+
+  if $LATEST ; then
+    docker tag "$DOCKER_IMAGE" "${DOCKER_IMAGE%:*}:latest"
+    docker push "${DOCKER_IMAGE%:*}:latest"
+  fi
 done
 
 echo "Push Blockchain docker image: $BLOCKCHAIN_IMAGE_NAME"
 docker push "$BLOCKCHAIN_IMAGE_NAME"
+
+if $LATEST ; then
+  docker tag "$BLOCKCHAIN_IMAGE_NAME" "${BLOCKCHAIN_IMAGE_NAME%:*}:latest"
+  docker push "${BLOCKCHAIN_IMAGE_NAME%:*}:latest"
+fi
