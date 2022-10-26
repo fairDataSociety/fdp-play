8ac3c6a6ba552c001177b5d42e549f88ab6ce4dc
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2022-10-26 11:43:20 +0200

    fix: waiting for postage batch usability

--- test/utils/docker.ts
+++ test/utils/docker.ts
@@ -10,16 +10,21 @@ export async function sleep(ms: number): Promise<void> {
   return new Promise<void>(resolve => setTimeout(() => resolve(), ms))
 }
 
-export async function waitForUsablePostageStamp(beeDebug: BeeDebug, id: BatchId, timeout = 120_000): Promise<void> {
+export async function waitForUsablePostageStamp(beeDebug: BeeDebug, id: BatchId, timeout = 30_000): Promise<void> {
   const TIME_STEP = 1500
   for (let time = 0; time < timeout; time += TIME_STEP) {
-    const stamp = await beeDebug.getPostageBatch(id)
+    // it is in a try...catch because after postage creation Bee (1.8.2) does not find stamp immediately somehow
+    try {
+      const stamp = await beeDebug.getPostageBatch(id)
 
-    if (stamp.usable) {
-      return
+      if (stamp.usable) {
+        return
+      }
+
+      await sleep(TIME_STEP)
+    } catch (e) {
+      await sleep(TIME_STEP)
     }
-
-    await sleep(TIME_STEP)
   }
 
   throw new Error('Timeout on waiting for postage stamp to become usable')
