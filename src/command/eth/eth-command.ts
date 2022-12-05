2e665b40643ac1f9c2087380ee8f6c85b6ae2fd0
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2022-12-05 19:13:13 +0100

    feat: network id check before eth related commands

--- src/command/eth/eth-command.ts
+++ src/command/eth/eth-command.ts
@@ -1,10 +1,19 @@
 import { RootCommand } from '../root-command'
 import { providers } from 'ethers'
+import { CommandLineError } from '../../utils/error'
 
 export class EthCommand extends RootCommand {
   protected provider = new providers.JsonRpcProvider('http://localhost:9545')
 
   protected async init(): Promise<void> {
     await super.init()
+
+    const network = await this.provider.getNetwork()
+
+    if (network.chainId !== 4020) {
+      throw new CommandLineError(
+        `The RPC connection operates on different network ID than the expected 4020. Got: ${network.chainId}`,
+      )
+    }
   }
 }
