61e108847962614ed5e8e2a8d263bb6403819d8b
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2022-12-07 16:05:41 +0100

    refactor: blockchain network id in the constant.ts

--- src/command/eth/eth-command.ts
+++ src/command/eth/eth-command.ts
@@ -1,7 +1,7 @@
 import { RootCommand } from '../root-command'
 import { providers } from 'ethers'
 import { CommandLineError } from '../../utils/error'
-import { BLOCKCHAIN_RPC_EP } from '../../constants'
+import { BLOCKCHAIN_NETWORK_ID, BLOCKCHAIN_RPC_EP } from '../../constants'
 
 export class EthCommand extends RootCommand {
   protected provider = new providers.JsonRpcProvider(BLOCKCHAIN_RPC_EP)
@@ -11,7 +11,7 @@ export class EthCommand extends RootCommand {
 
     const network = await this.provider.getNetwork()
 
-    if (network.chainId !== 4020) {
+    if (network.chainId !== BLOCKCHAIN_NETWORK_ID) {
       throw new CommandLineError(
         `The RPC connection operates on different network ID than the expected 4020. Got: ${network.chainId}`,
       )
--- src/constants.ts
+++ src/constants.ts
@@ -7,3 +7,4 @@ export const DEFAULT_BLOCKCHAIN_CONTAINER = 'fdp-play-blockchain'
 export const DEFAULT_FAIROS_IMAGE = 'fairdatasociety/fairos-dfs'
 export const ENV_ENV_PREFIX_KEY = 'FDP_PLAY_ENV_PREFIX'
 export const BLOCKCHAIN_RPC_EP = 'http://localhost:9545'
+export const BLOCKCHAIN_NETWORK_ID = 4020
