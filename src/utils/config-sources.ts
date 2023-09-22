8045902f046068dc3aa2f94e85a92754f3fbdffc
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2023-09-22 13:10:22 +0200

    refactor: remove version format check

--- src/utils/config-sources.ts
+++ src/utils/config-sources.ts
@@ -1,12 +1,6 @@
-const VERSION_REGEX = /^\d+\.\d+\.\d+(-\w+)*$/
-
 export function stripCommit(version: string): string {
   if (version === 'latest') {
     return version
-  }
-
-  if (!VERSION_REGEX.test(version)) {
-    throw new Error('The version does not have expected format!')
   }
 
   // If the version contains commit ==> hash remove it
