48adc2e9e39768f6c13eb8983e981c38106eadeb
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2023-10-11 11:19:38 +0200

    feat: blockchain build refactor

--- orchestrator/builder/blockchain/init.sh
+++ orchestrator/builder/blockchain/init.sh
@@ -3,7 +3,5 @@
 MY_PATH=$(dirname "$0")
 MY_PATH=$( cd "$MY_PATH" && pwd )
 
-docker run --rm -v $MY_PATH:/root ethereum/client-go account new --password /root/password
-read -p "Update genesis.json and start.sh with the generated address before moving on" continue
 docker run -v $MY_PATH:/root ethereum/client-go init /root/genesis.json
-echo "Build the docker image of the blockchain with docker-build.sh"
+echo "Build the docker image of the blockchain with docker-build.sh after migrating and supplying"
