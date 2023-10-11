8ad3a21ffb431d8e912f8c086f80f7b56291cf8e
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2023-10-11 15:29:44 +0200

    feat: verification

--- orchestrator/hardhat.config.ts
+++ orchestrator/hardhat.config.ts
@@ -2,6 +2,8 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 import '@nomicfoundation/hardhat-ethers'
 
+const FDP_PLAY_URL = process.env.FDP_PLAY_URL || 'http://127.0.0.1'
+
 const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.19',
@@ -19,6 +21,21 @@ const config: HardhatUserConfig = {
       gasPrice: 10000000000, //10 gwei
       accounts: ['0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'],
     },
+  },
+  etherscan: {
+    apiKey: {
+      fdpPlay: 'hello_there!',
+    },
+    customChains: [
+      {
+        network: 'fdpPlay',
+        chainId: 4020,
+        urls: {
+          apiURL: `${FDP_PLAY_URL}/api`,
+          browserURL: `${FDP_PLAY_URL}`,
+        },
+      },
+    ],
   },
 }
 
