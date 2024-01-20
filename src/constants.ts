70afb77cb5bb15dfadef5ef866a0f49589b5e44d
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2024-01-20 03:58:00 +0100

    fix: default blockchain image

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
  * for base image that this project build use "fdp-play-blockchain:1.2.0"
  * it will use the latest tag. on image upgrade, either delete the latest locally or force pull
  */
-export const DEFAULT_BLOCKCHAIN_IMAGE = 'fairdatasociety/fdp-contracts-blockchain'
+export const DEFAULT_BLOCKCHAIN_IMAGE = 'fairdatasociety/fdp-play-blockchain'
 export const DEFAULT_BLOCKCHAIN_CONTAINER = 'fdp-play-blockchain'
 export const DEFAULT_FAIROS_IMAGE = 'fairdatasociety/fairos-dfs'
 export const ENV_ENV_PREFIX_KEY = 'FDP_PLAY_ENV_PREFIX'
