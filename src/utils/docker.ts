a0fb29f1ff5561861dc06ededaa80b27ef7a5fbf
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2024-03-02 18:19:28 +0100

    fix: after dockerode update

--- src/utils/docker.ts
+++ src/utils/docker.ts
@@ -316,25 +316,16 @@ export class Docker {
     }
   }
 
-  public async logs(
-    target: ContainerType,
-    outputStream: NodeJS.WriteStream,
-    follow = false,
-    tail?: number,
-  ): Promise<void> {
+  public async logs(target: ContainerType, outputStream: NodeJS.WriteStream, tail?: number): Promise<void> {
     const { container } = await this.findContainer(this.getContainerName(target))
 
     if (!container) {
       throw new Error('Queen container does not exists, even though it should have had!')
     }
 
-    const logs = await container.logs({ stdout: true, stderr: true, follow, tail })
+    const logs = await container.logs({ stdout: true, stderr: true, tail })
 
-    if (!follow) {
-      outputStream.write(logs as unknown as Buffer)
-    } else {
-      logs.pipe(outputStream)
-    }
+    outputStream.write(logs as unknown as Buffer)
   }
 
   public async stopAll(allWithPrefix = false, deleteContainers = false): Promise<void> {
